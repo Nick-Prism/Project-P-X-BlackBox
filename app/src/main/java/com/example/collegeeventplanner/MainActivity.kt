595e12b3f3c723066857dd5b1c99c3319142e0f2
Nick-Prism <alok.cs23@bmsce.ac.in>
2025-04-06 14:54:18 +0530

    Add app/src/main/java/com/example/collegeeventplanner/MainActivity.kt 1743931461617

---
+++ app/src/main/java/com/example/collegeeventplanner/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.collegeeventplanner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.collegeeventplanner.navigation.NavGraph
+import com.example.collegeeventplanner.ui.theme.CollegeEventPlannerTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CollegeEventPlannerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
